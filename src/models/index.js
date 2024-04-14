60a85771a201716bcae59597f78d4bbc51363d8f
shabarshov <shabarshov3228@mail.ru>
2024-04-14 19:27:19 +0300

    feat(users): add cascade delete

--- src/models/index.js
+++ src/models/index.js
@@ -3,13 +3,19 @@ const Song = require("./Song")
 const Album = require("./Album")
 const History = require("./History")
 
-User.hasMany(Album)
+User.hasMany(Album, {
+  onDelete: "cascade",
+})
 Album.belongsTo(User)
 
-User.hasMany(History)
+User.hasMany(History, {
+  onDelete: "cascade",
+})
 History.belongsTo(User)
 
-User.hasMany(Song)
+User.hasMany(Song, {
+  onDelete: "cascade",
+})
 Song.belongsTo(User)
 
 module.exports = {
