ae9410e88002e6bda20703ea35d736787ab8a19c
shabarshov <shabarshov3228@mail.ru>
2024-04-25 06:55:19 +0300

    add getUserController

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const createUserController = require("#controllers/users/createUserController")
 const deleteUserController = require("#controllers/users/deleteUserController")
 const updateUserController = require("#controllers/users/updateUserController")
+const getUserController = require("#controllers/users/getUserController")
 
 router.post("/", createUserController)
 router.delete("/:userId", deleteUserController)
 router.patch("/:userId", updateUserController)
+router.get("/:userId", getUserController)
 
 module.exports = router
